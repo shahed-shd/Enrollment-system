5a45dce647e34ac548a4abaf80b8157b07bbb787
Shahed-shd <shahed.shd777@gmail.com>
2017-07-23 21:28:35 +0600

    Studen info input is going on.

--- main.py
+++ main.py
@@ -10,13 +10,72 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
+from kivy.uix.spinner import Spinner
 from kivy.uix.textinput import TextInput
 from kivy.graphics import Color, Rectangle
+
+
+class StudentInfoInputLayout(RelativeLayout):
+    def __init__(self, **kwargs):
+        super(StudentInfoInputLayout, self).__init__(**kwargs)
+
+        n = 14
+        idx = n
+
+        idx -= 1
+        self.label_rollno = Label(text="Roll no:", italic=True, size_hint=(0.25, 1/n), pos_hint={'x': 0, 'y': 1/n*idx})
+        self.text_input_rollno = TextInput(text='1437', hint_text='Enter roll no here.', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1/n), pos_hint={'x': 0.5, 'y': 1/n*idx})
+
+        idx -= 1
+        self.label_name = Label(text="Name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        self.text_input_firstname = TextInput(text='', hint_text='First name', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
+        self.text_input_lastname = TextInput(text='', hint_text='last name', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1 / n), pos_hint={'x': 0.5, 'y': 1 / n * idx})
+
+        idx -= 1
+        self.label_fathersname = Label(text="Father's name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        self.text_input_fathersname = TextInput(text='', hint_text='Enter here.', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1 / n), pos_hint={'x': 0.5, 'y': 1 / n * idx})
+
+        idx -= 1
+        self.label_mothersname = Label(text="Mother's name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        self.text_input_mothersname = TextInput(text='', hint_text='Enter here.', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1 / n), pos_hint={'x': 0.5, 'y': 1 / n * idx})
+
+        idx -= 1
+        self.label_gender = Label(text="Gender:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        self.spinner_gender = Spinner(text='Male', values=('Male', 'Female', 'Other'), size_hint=(0.25, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
+
+        self.label_bloodgroup = Label(text="Blood group:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0.5, 'y': 1 / n * idx})
+        self.spinner_bloodgroup = Spinner(text='O+', values=('A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-'), size_hint=(0.25, 1 / n), pos_hint={'x': 0.75, 'y': 1 / n * idx})
+
+        idx -= 1
+        self.label_dob = Label(text="Date of birth:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        self.text_input_dob_day = TextInput(text='', hint_text='DD', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
+        self.text_input_dob_month = TextInput(text='', hint_text='MM', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1 / n), pos_hint={'x': 0.5, 'y': 1 / n * idx})
+        self.text_input_dob_year = TextInput(text='', hint_text='YYYY', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.25, 1 / n), pos_hint={'x': 0.75, 'y': 1 / n * idx})
+
+        self.add_widget(self.label_rollno)
+        self.add_widget(self.text_input_rollno)
+        self.add_widget(self.label_name)
+        self.add_widget(self.text_input_firstname)
+        self.add_widget(self.text_input_lastname)
+        self.add_widget(self.label_fathersname)
+        self.add_widget(self.text_input_fathersname)
+        self.add_widget(self.label_mothersname)
+        self.add_widget(self.text_input_mothersname)
+        self.add_widget(self.label_gender)
+        self.add_widget(self.spinner_gender)
+        self.add_widget(self.label_bloodgroup)
+        self.add_widget(self.spinner_bloodgroup)
+        self.add_widget(self.label_dob)
+        self.add_widget(self.text_input_dob_day)
+        self.add_widget(self.text_input_dob_month)
+        self.add_widget(self.text_input_dob_year)
 
 
 class HomeScreenLayout(FloatLayout):
     def __init__(self, **kwargs):
         super(HomeScreenLayout, self).__init__(**kwargs)
+
+        self.add_widget(StudentInfoInputLayout())
 
 
 class StartScreenLayout(FloatLayout):
@@ -60,6 +119,8 @@ class MainScreenManager(ScreenManager):
 
         self.add_widget(start_scr)
         self.add_widget(home_scr)
+
+        # self.current = 'home_screen'
 
 
 class EnrollmentSystem(App):
