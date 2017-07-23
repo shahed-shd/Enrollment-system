b638c70cb9ed6d3db5096f380ac625e0f3e6ab77
Shahed-shd <shahed.shd777@gmail.com>
2017-07-23 18:35:42 +0600

    Start screen done.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.graphics import Color, Rectangle
 
@@ -17,56 +18,47 @@ class HomeScreenLayout(FloatLayout):
     def __init__(self, **kwargs):
         super(HomeScreenLayout, self).__init__(**kwargs)
 
-        # Canvas color
+
+class StartScreenLayout(FloatLayout):
+    def __init__(self, **kwargs):
+        super(StartScreenLayout, self).__init__(**kwargs)
+
+        # Canvas color.
         with self.canvas.before:
             Color(0.25, 0, 0, 1)
             self.rect = Rectangle(size=self.size, pos=self.pos)
         self.bind(size=lambda *a: setattr(self.rect, 'size', self.size), pos=lambda *a: setattr(self.rect, 'pos', self.pos))
 
-        # Popup feature.
-        self.popup_create_new_system = Popup(title='Create new system', title_color=(0, 1, 1, 1), separator_height=2, title_align='center', size_hint=(0.9, 0.5), auto_dismiss=False)
-
-        layout = RelativeLayout()
-        text_input = TextInput(text='', hint_text="Enter system name here", focus=False, multiline=False, write_tab=False, size_hint=(0.75, 0.2), pos_hint={'x': 0.125, 'y': 0.65})
-        text_input.bind(text=self.space_to_underscore)
-        btn_create = Button(text='Create', on_release=self.create_new_system, size_hint=(0.25, 0.2), pos_hint={'x': 0.25, 'y': 0.25})
-        btn_cancel = Button(text='Cancel', on_release=self.popup_create_new_system.dismiss, size_hint=(0.25, 0.2), pos_hint={'x': 0.5, 'y': 0.25})
-
-        layout.add_widget(text_input)
-        layout.add_widget(btn_create)
-        layout.add_widget(btn_cancel)
-
-        self.popup_create_new_system.content = layout
-        self.popup_text_input = text_input
-
-        # Adding widgets.
-        self.add_widget(Label(text='Systems:', italic=True, bold=True, font_size=30, size_hint=(1, 0.1), pos_hint={'x': 0, 'y': 0.9}))
-        self.add_widget(Button(text='Create new system', on_release=self.popup_create_new_system.open, size_hint=(0.5, 0.1), pos_hint={'x': 0.25, 'y': 0.8}))
+        # adding widgets.
+        self.add_widget(Image(source='images/IST_logo.png', allow_stretch=True, keep_ratio=False, color=(0.5, 0.5, 0.5, 1), size_hint=(0.5, 0.5), pos_hint={'x': 0.25, 'y': 0.45}))
+        self.add_widget(Label(text='Admin password:', bold=True, italic=True, size_hint=(0.5, 0.075), pos_hint={'x': 0.25, 'y': .25}))
+        self.text_input = TextInput(text='', hint_text='Enter password here.', password=True, multiline=False, write_tab=False, focus=False, size_hint=(0.5, 0.075), pos_hint={'x': 0.25, 'y': 0.175})
+        self.add_widget(self.text_input)
+        self.add_widget(Button(text='Submit', bold=True, italic=True, on_release=self.perform_submit, size_hint=(0.15, 0.075), pos_hint={'x': 0.35, 'y': 0.075}))
+        self.add_widget(Button(text='Reset', bold=True, italic=True, on_release=lambda *a: setattr(self.text_input, 'text', ''), size_hint=(0.15, 0.075), pos_hint={'x': 0.5, 'y': 0.075}))
 
 
-    def space_to_underscore(self, *a):
-        if len(self.popup_text_input.text) > 0 and self.popup_text_input.text[-1] == ' ':
-            self.popup_text_input.text = self.popup_text_input.text[:-1] + '_'
-
-
-    def create_new_system(self, *a):
-        if len(self.popup_text_input.text) > 0:
-            print("New system name:", self.popup_text_input.text)
-            self.popup_text_input.text = ''
-            self.popup_create_new_system.dismiss()
-            # sql task here
+    def perform_submit(self, *a):
+        if self.text_input.text == '12345':
+            mngr = self.parent.manager
+            mngr.transition.direction = 'left'
+            mngr.current = 'home_screen'
 
 
 class MainScreenManager(ScreenManager):
     def __init__(self, **kwargs):
         super(MainScreenManager, self).__init__(**kwargs)
 
+        start_scr = Screen(name='start_screen')
         home_scr = Screen(name='home_screen')
 
+        self.start_screen_layout = StartScreenLayout()
         self.home_screen_layout = HomeScreenLayout()
 
+        start_scr.add_widget(self.start_screen_layout)
         home_scr.add_widget(self.home_screen_layout)
 
+        self.add_widget(start_scr)
         self.add_widget(home_scr)
 
 
