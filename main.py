450704c5bca08454520e5f7b5a9f086cf00edd30
Shahed-shd <shahed.shd777@gmail.com>
2017-07-27 04:22:40 +0600

    FIND TAB almost done.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import datetime
 import os
 import re
 import shutil
-from functools import partial
+from enum import Enum, unique
 
 from kivy.app import App
 from kivy.cache import Cache
@@ -113,50 +113,50 @@ class StudentInfoInputLayout(RelativeLayout):
         idx = n
 
         idx -= 1
-        self.label_name = Label(text="Name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_name = Label(text="Name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_firstname = TextInput(text='', hint_text='First name', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.37, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
         self.text_input_lastname = TextInput(text='', hint_text='last name', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.37, 1 / n), pos_hint={'x': 0.62, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_fathersname = Label(text="Father's name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_fathersname = Label(text="Father's name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_fathersname = TextInput(text='', hint_text="Father's name", password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.74, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_mothersname = Label(text="Mother's name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_mothersname = Label(text="Mother's name:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_mothersname = TextInput(text='', hint_text="Mother's name", password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.74, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_gender = Label(text="Gender:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_gender = Label(text="Gender:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.spinner_gender = Spinner(text='Male', values=('Male', 'Female', 'Other'), size_hint=(0.5, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_bloodgroup = Label(text="Blood group:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_bloodgroup = Label(text="Blood group:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.spinner_bloodgroup = Spinner(text='O+', values=('A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-'), size_hint=(0.5, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_dob = Label(text="Date of birth:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_dob = Label(text="Date of birth:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_dob_day = TextInput(text='', hint_text='DD', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.12, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
         self.text_input_dob_month = TextInput(text='', hint_text='MM', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.12, 1 / n), pos_hint={'x': 0.37, 'y': 1 / n * idx})
         self.text_input_dob_year = TextInput(text='', hint_text='YYYY', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.25, 1 / n), pos_hint={'x': 0.49, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_address = Label(text="Address:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_address = Label(text="Address:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_address = TextInput(text='', hint_text='Address', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.74, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_nationality = Label(text="Nationality:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_nationality = Label(text="Nationality:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_nationality = TextInput(text='', hint_text='Nationality', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.5, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_email_address = Label(text="Email address:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_email_address = Label(text="Email address:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_email_address = TextInput(text='', hint_text='Email address', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.74, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_phone_no = Label(text="Phone no.:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_phone_no = Label(text="Phone no.:", italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_phone_no = TextInput(text='', hint_text='Phone no.', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.5, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_ssc = Label(text='SSC:', italic=True, size_hint=(0.16, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_ssc = Label(text='SSC:', italic=True, size_hint=(0.16, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_ssc_roll = TextInput(text='', hint_text='Roll no', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.16, 1 / n), pos_hint={'x': 0.16, 'y': 1 / n * idx})
         self.text_input_ssc_reg = TextInput(text='', hint_text='Reg no', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.16, 1 / n), pos_hint={'x': 0.32, 'y': 1 / n * idx})
         self.text_input_ssc_gpa = TextInput(text='', hint_text='GPA', password=False, multiline=False, write_tab=False, focus=False, input_filter='float', size_hint=(0.16, 1 / n), pos_hint={'x': 0.48, 'y': 1 / n * idx})
@@ -164,7 +164,7 @@ class StudentInfoInputLayout(RelativeLayout):
         self.spinner_ssc_board = Spinner(text='Dhaka', values=('Dhaka', 'Rajshahi', 'Comilla', 'Jessore', 'Chittagong', 'Barisal', 'Sylhet', 'Dinajpur', 'Madrasah'), size_hint=(0.16, 1 / n), pos_hint={'x': 0.80, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_hsc = Label(text='HSC:', italic=True, size_hint=(0.16, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_hsc = Label(text='HSC:', italic=True, size_hint=(0.16, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.text_input_hsc_roll = TextInput(text='', hint_text='Roll no', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.16, 1 / n), pos_hint={'x': 0.16, 'y': 1 / n * idx})
         self.text_input_hsc_reg = TextInput(text='', hint_text='Reg no', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.16, 1 / n), pos_hint={'x': 0.32, 'y': 1 / n * idx})
         self.text_input_hsc_gpa = TextInput(text='', hint_text='GPA', password=False, multiline=False, write_tab=False, focus=False, input_filter='float', size_hint=(0.16, 1 / n), pos_hint={'x': 0.48, 'y': 1 / n * idx})
@@ -172,51 +172,51 @@ class StudentInfoInputLayout(RelativeLayout):
         self.spinner_hsc_board = Spinner(text='Dhaka', values=('Dhaka', 'Rajshahi', 'Comilla', 'Jessore', 'Chittagong', 'Barisal', 'Sylhet', 'Dinajpur', 'Madrasah'), size_hint=(0.16, 1 / n), pos_hint={'x': 0.80, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_dept = Label(text='Department:', italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
+        label_dept = Label(text='Department:', italic=True, size_hint=(0.25, 1 / n), pos_hint={'x': 0, 'y': 1 / n * idx})
         self.spinner_dept = Spinner(text='CSE', values=('CSE', 'ECE', 'BBA'), size_hint=(0.5, 1 / n), pos_hint={'x': 0.25, 'y': 1 / n * idx})
 
         idx -= 1
-        self.label_rollno = Label(text="Roll no:", italic=True, size_hint=(0.25, 1/n), pos_hint={'x': 0, 'y': 1/n*idx})
+        label_rollno = Label(text="Roll no:", italic=True, size_hint=(0.25, 1/n), pos_hint={'x': 0, 'y': 1/n*idx})
         self.text_input_rollno = TextInput(text='', hint_text='Roll no.', password=False, multiline=False, write_tab=False, focus=False, input_filter='int', size_hint=(0.25, 1/n), pos_hint={'x': 0.25, 'y': 1/n*idx})
 
-        self.add_widget(self.label_name)
+        self.add_widget(label_name)
         self.add_widget(self.text_input_firstname)
         self.add_widget(self.text_input_lastname)
-        self.add_widget(self.label_fathersname)
+        self.add_widget(label_fathersname)
         self.add_widget(self.text_input_fathersname)
-        self.add_widget(self.label_mothersname)
+        self.add_widget(label_mothersname)
         self.add_widget(self.text_input_mothersname)
-        self.add_widget(self.label_gender)
+        self.add_widget(label_gender)
         self.add_widget(self.spinner_gender)
-        self.add_widget(self.label_bloodgroup)
+        self.add_widget(label_bloodgroup)
         self.add_widget(self.spinner_bloodgroup)
-        self.add_widget(self.label_dob)
+        self.add_widget(label_dob)
         self.add_widget(self.text_input_dob_day)
         self.add_widget(self.text_input_dob_month)
         self.add_widget(self.text_input_dob_year)
-        self.add_widget(self.label_address)
+        self.add_widget(label_address)
         self.add_widget(self.text_input_address)
-        self.add_widget(self.label_nationality)
+        self.add_widget(label_nationality)
         self.add_widget(self.text_input_nationality)
-        self.add_widget(self.label_email_address)
+        self.add_widget(label_email_address)
         self.add_widget(self.text_input_email_address)
-        self.add_widget(self.label_phone_no)
+        self.add_widget(label_phone_no)
         self.add_widget(self.text_input_phone_no)
-        self.add_widget(self.label_ssc)
+        self.add_widget(label_ssc)
         self.add_widget(self.text_input_ssc_roll)
         self.add_widget(self.text_input_ssc_reg)
         self.add_widget(self.text_input_ssc_gpa)
         self.add_widget(self.text_input_ssc_year)
         self.add_widget(self.spinner_ssc_board)
-        self.add_widget(self.label_hsc)
+        self.add_widget(label_hsc)
         self.add_widget(self.text_input_hsc_roll)
         self.add_widget(self.text_input_hsc_reg)
         self.add_widget(self.text_input_hsc_gpa)
         self.add_widget(self.text_input_hsc_year)
         self.add_widget(self.spinner_hsc_board)
-        self.add_widget(self.label_dept)
+        self.add_widget(label_dept)
         self.add_widget(self.spinner_dept)
-        self.add_widget(self.label_rollno)
+        self.add_widget(label_rollno)
         self.add_widget(self.text_input_rollno)
 
 
@@ -277,7 +277,7 @@ class FileChooserPopup(Popup):
         self.dismiss()
 
 
-class StudentProfileLayout(RelativeLayout):
+class StudentProfileLayout(RelativeLayout, kivy.uix.widget.Widget):
     def __init__(self, **kwargs):
         super(StudentProfileLayout, self).__init__(**kwargs)
 
@@ -285,6 +285,8 @@ class StudentProfileLayout(RelativeLayout):
         self.profile_pic = Image(allow_stretch=True, keep_ratio=False, size_hint=(0.20, 0.40), pos_hint={'x': 0.025, 'y': 0.55})    # source will be add in attach_student()
         self.student_info_input_layout = StudentInfoInputLayout(size_hint=(0.75, 0.75), pos_hint={'x': 0.25, 'y': 0.25})
         self.btn_cancel = Button(text='Cancel', italic=True, on_release=self.btn_cancel_do, size_hint=(0.2, 0.06), pos_hint={'x': 0.75, 'y': 0.08})
+        self.btn_ok_update = Button(text='OK', italic=True, on_release=self.btn_ok_update_do, size_hint=(0.2, 0.06), pos_hint={'x': 0.55, 'y': 0.08})
+        self.btn_undo_changes = Button(text='Undo changes', italic=True, on_release=self.assign_student_info, opacity=0.25, size_hint=(0.2, 0.06), pos_hint={'x': 0.35, 'y': 0.08})
 
         self.text_input_file_choose = TextInput(text='', hint_text='No file selected', readonly=True, size_hint=(0.6, 0.057), pos_hint={'x': 0.25, 'y': 0.18})
         self.file_chooser_popup = FileChooserPopup(size_hint=(0.8, 0.8), pos_hint={'center_x': 0.5, 'center_y': 0.5})
@@ -298,6 +300,115 @@ class StudentProfileLayout(RelativeLayout):
         self.add_widget(Button(text='Choose', italic=True, on_release=self.file_chooser_popup.open, size_hint=(0.14, 0.057), pos_hint={'x': 0.85, 'y': 0.18}))
 
         self.add_widget(self.btn_cancel)
+        self.add_widget(self.btn_ok_update)
+        self.add_widget(self.btn_undo_changes)
+
+        for ch in self.student_info_input_layout.children:
+            if type(ch) != Label:
+                ch.bind(text=self.any_change_do)
+
+        self.text_input_file_choose.bind(text=self.any_change_do)
+
+
+    def check_any_change(self, *a):
+        inp = self.student_info_input_layout
+        student = self.student
+
+        if inp.text_input_firstname.text != student.first_name:
+            return True
+
+        if inp.text_input_lastname.text != student.last_name:
+            return True
+
+        if inp.text_input_fathersname.text != student.fathers_name:
+            return True
+
+        if inp.text_input_mothersname.text != student.mothers_name:
+            return True
+
+        if inp.spinner_gender.text != student.gender:
+            return True
+
+        if inp.spinner_bloodgroup.text != student.blood_group:
+            return True
+
+        if inp.text_input_dob_day.text != str(student.date_of_birth.day):
+            return True
+
+        if inp.text_input_dob_month.text != str(student.date_of_birth.month):
+            return True
+
+        if inp.text_input_dob_year.text != str(student.date_of_birth.year):
+            return True
+
+        if inp.text_input_address.text != student.address:
+            return True
+
+        if inp.text_input_nationality.text != student.nationality:
+            return True
+
+        if inp.text_input_email_address.text != student.email_address:
+            return True
+
+        if inp.text_input_phone_no.text != student.phone_no:
+            return True
+
+        if inp.text_input_ssc_roll.text != str(student.ssc_roll_no):
+            return True
+
+        if inp.text_input_ssc_reg.text != str(student.ssc_reg_no):
+            return True
+
+        if inp.text_input_ssc_gpa.text != str(student.ssc_gpa):
+            return True
+
+        if inp.text_input_ssc_year.text != str(student.ssc_year):
+            return True
+
+        if inp.spinner_ssc_board.text != student.ssc_board:
+            return True
+
+        if inp.text_input_hsc_roll.text != str(student.hsc_roll_no):
+            return True
+
+        if inp.text_input_hsc_reg.text != str(student.hsc_reg_no):
+            return True
+
+        if inp.text_input_hsc_gpa.text != str(student.hsc_gpa):
+            return True
+
+        if inp.text_input_hsc_year.text != str(student.hsc_year):
+            return True
+
+        if inp.spinner_hsc_board.text != student.hsc_board:
+            return True
+
+        if inp.spinner_dept.text != student.dept:
+            return True
+
+        if inp.text_input_rollno.text != str(student.roll_no):
+            return True
+
+        if self.text_input_file_choose.text != student.photo_path:
+            return True
+
+        return False
+
+
+    def any_change_do(self, *a):
+        if self.student == None:
+            return
+
+        if self.check_any_change():
+            self.btn_undo_changes.opacity = 1
+            self.btn_ok_update.text = 'Update'
+        else:
+            self.btn_undo_changes.opacity = 0.25
+            self.btn_ok_update.text = 'OK'
+
+
+    def btn_ok_update_do(self, *a):
+        pass
 
 
     def btn_cancel_do(self, *a):
